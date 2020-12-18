8acb6e718546434f22c5df0817e6aa4a31d17699
steuwe <52547093+steuwe@users.noreply.github.com>
2020-12-18 22:30:16 +0100

    Update localizationErrors.py

--- analysisAPI/localizationErrors.py
+++ analysisAPI/localizationErrors.py
@@ -173,6 +173,8 @@ def localizationErrors( coco_analyze, imgs_info, saveDir ):
             x = kp[0::3]; y = kp[1::3]; v = kp[2::3]
 
             # show the image
+            print('*******************************')
+            print(imgs_info[t['image_id']])
             I = io.imread('/content/drive/My Drive/split_daten_valid/Images/' + str(imgs_info[t['image_id']]['file_name']))
             plt.figure(figsize=(10,10)); plt.axis('off')
             plt.imshow(I)
