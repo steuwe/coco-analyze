2f83567d9ba6e047fbe8f32f0f1b78f1b247a789
steuwe <52547093+steuwe@users.noreply.github.com>
2020-12-20 14:47:22 +0100

    Update utilities.py

--- analysisAPI/utilities.py
+++ analysisAPI/utilities.py
@@ -28,7 +28,7 @@ def show_dets(coco_dts, coco_gts, img_info, save_path=None):
     if len(coco_dts) == 0 and len(coco_gts)==0:
         return 0
 
-    I = io.imread('/content/drive/My Drive/split_daten_valid/Images' + str(img_info['coco_url']))
+    I = io.imread('/content/drive/My Drive/split_daten_valid/Images/' + str(img_info['coco_url']))
     plt.figure(figsize=(10,10)); plt.axis('off')
     plt.imshow(I)
     ax = plt.gca(); ax.set_autoscale_on(False)
