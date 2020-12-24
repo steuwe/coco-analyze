ab830d23ca481c9b4b2e498765dc876556d78383
steuwe <52547093+steuwe@users.noreply.github.com>
2020-12-25 00:18:01 +0100

    Update cocoeval.py

--- pycocotools/cocoeval.py
+++ pycocotools/cocoeval.py
@@ -620,7 +620,7 @@ class Params:
         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
         self.iouThrs = np.linspace(.5, 0.95, int (np.round((0.95 - .5) / .05)) + 1, endpoint=True)
         self.recThrs = np.linspace(.0, 1.00, int (np.round((1.00 - .0) / .01)) + 1, endpoint=True)
-        self.maxDets = [20]
+        self.maxDets = [200]
         self.areaRng = [[0 ** 2, 1e5 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
         self.areaRngLbl = ['all', 'medium', 'large']
         self.useCats = 1
