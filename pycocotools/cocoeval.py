4e165d29f8ca66388eeb1aa491e8e36af3d6c3bb
steuwe <52547093+steuwe@users.noreply.github.com>
2020-12-24 18:11:35 +0100

    Update cocoeval.py

--- pycocotools/cocoeval.py
+++ pycocotools/cocoeval.py
@@ -167,6 +167,7 @@ class COCOeval:
                  for areaRng in p.areaRng
                  for imgId in p.imgIds
              ]
+        print(len(self.evalImgs))
         self._paramsEval = copy.deepcopy(self.params)
         toc = time.time()
         print('<{}:{}>DONE (t={:0.2f}s).'.format(__author__,__version__,toc-tic))
